68bed77270775eed05682dc94d626dfa5df1a9ea
Sahil Guglani <103830326+AbstractDeveloper@users.noreply.github.com>
2023-02-18 10:40:48 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const port = 5000;
+
+const Device = require('./models/device');
+
+mongoose.connect('mongodb+srv://Sahil:Sahil@cluster0.4jhqekj.mongodb.net/mydb', 
+{ useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/api/test', (req, res) => {
+  res.send('The API is working!');
+});
+
+app.get('/api/devices', (req, res) => {
+  Device.find({}, (err, devices) => {
+    if (err == true) {
+      return res.send(err);
+    } else {
+      return res.send(devices);
+    }
+  });
+});
+
+app.post('/api/devices', (req, res) => {
+  const { name, user, sensorData } = req.body;
+  const newDevice = new Device({
+    name,
+    user,
+    sensorData
+  });
+  newDevice.save(err => {
+    return err
+      ? res.send(err)
+      : res.send('successfully added device and data');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+
+
+
+// app.get('/api/devices', (req, res) => {
+//   Device.find({}, (err, devices) => {
+//     console.log(devices);
+//   });
+// });
+
+// app.get('/api/devices', (req, res) => {
+//   Device.find({}, (err, devices) => {
+//     return res.send(devices);
+//   });
+// });
+
+// app.post('/api/devices', (req, res) => {
+//   const { name, user, sensorData } = req.body;
+//   const newDevice = new Device({
+//     name,
+//     user,
+//     sensorData
+//   });
+//   newDevice.save(err => {
+//   });
+// });
+
